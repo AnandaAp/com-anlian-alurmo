397b4ddac72dee509a397884d424b2fa3e25f1ce
AnandaAp <ananda.apriliansyah@ti.ukdw.ac.id>
2023-03-04 23:34:06 +0700

    master

---
+++ app/src/main/kotlin/com/anlian/alurmo/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.anlian.alurmo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Dark Mode Color Pallete
+ */
+val RubyRed = Color(0xFFa4031f)
+val Carmine = Color(0xFF8C031A)
+val SandyBrown = Color(0xFFF2A359)
+
+/**
+ * Dark Mode Color Pallet
+ */
+val MediumCandyAppleRed = Color(0xFFDC0428)
+val FireEngineRed = Color(0xFFc80425)
+val MellowApricot = Color(0xFFF4B67B)
