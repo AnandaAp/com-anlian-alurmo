cee6cd6282b0612f88e5054ae3fb5b8c00502d50
Ananda Apriliansyah Cahya Utama <42529423+AnandaAp@users.noreply.github.com>
2023-03-05 09:03:20 +0700

    23.3/integration (#3)
    * 23.3/integration
    
    1. Update build.gradle to build.gradle.kts
    2. update configuration from gradle to kotlin
    3. set version as dynamic as possible
    4. Implement worker
    5. Implement hilt-dagger
    6. Update android.yml
    7. update lint configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 buildscript {
     dependencies {
-        val gms_version = "4.3.15"
-        classpath("com.google.gms:google-services:$gms_version")
+        val gmsVersion = "4.3.15"
+        classpath("com.google.gms:google-services:$gmsVersion")
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    val app_version = "8.1.0-alpha07"
-    val kotlin_version = "1.8.10"
-    val ksp_version = "1.8.10-1.0.9"
-    val hilt_version = "2.44"
+    val appVersion = "8.1.0-alpha07"
+    val kotlinVersion = "1.8.10"
+    val kspVersion = "1.8.10-1.0.9"
+    val hiltVersion = "2.44"
 
-    id("com.android.application") version "$app_version" apply false
-    id("com.android.library") version "$app_version" apply false
-    id("org.jetbrains.kotlin.android") version "$kotlin_version" apply false
-    id("com.google.devtools.ksp") version "$ksp_version" apply false
-    id("com.google.dagger.hilt.android") version "$hilt_version" apply false
+    id("com.android.application") version appVersion apply false
+    id("com.android.library") version appVersion apply false
+    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
+    id("com.google.devtools.ksp") version kspVersion apply false
+    id("com.google.dagger.hilt.android") version hiltVersion apply false
 }
 
 //tasks.register("clean", Delete) {
